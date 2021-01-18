cc2ecf09136f7fb234465ba0db460ce2408778f8
ZalmanKelber <62047666+ZalmanKelber@users.noreply.github.com>
2021-01-17 21:13:20 -0500

    configured graphql

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 dotenv.config();
 const app = express();
+
 const mongoUri = `mongodb+srv://ZalmanKelber:${process.env.PASSWORD}@cluster0.ativt.mongodb.net/indoeuropeandev?retryWrites=true&w=majority`
 mongoose.connect(mongoUri, {
     useNewUrlParser: true,
@@ -11,9 +14,27 @@ mongoose.connect(mongoUri, {
     useUnifiedTopology: true
 }).then(() => console.log("MongoDB connected")).catch(err => console.log("error:", err));
 
+const schema = buildSchema(`
+    type Query {
+        name: String
+    }
+`)
+
+const rootValue = {
+    name: () => {
+        return "Indo-European Languages";
+    }
+}
+
 app.get("/", (req, res) => {
     res.send("Hello from Server");
-})
+});
+
+app.use("/graphql", graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql: true
+}));
 
 const PORT = process.env.PORT || 5000;
 
